ec64178ebd7e5149138dcd6e469055bfc3d1bd6a
Catevika <dominique.bello@outlook.fr>
2019-07-13 17:05:55 +0200

    Updates

---
+++ precache-manifest.52d230fc1270721bc3ab1457b326cbdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c944283f33ece49c41d46a9c300c98e",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/index.html"
+  },
+  {
+    "revision": "34efa1ffeb1931b12efe",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "00a552126ee6c4b692ab",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/static/css/main.d1e4dc2f.chunk.css"
+  },
+  {
+    "revision": "34efa1ffeb1931b12efe",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/static/js/2.62e2bba4.chunk.js"
+  },
+  {
+    "revision": "00a552126ee6c4b692ab",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/static/js/main.00e6efe0.chunk.js"
+  },
+  {
+    "revision": "f53afe48eab648278999",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/static/js/runtime~main.28ce1bfa.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/UDEMY_ColorPalettes_CapstoneProject/static/media/bg.77428cd4.svg"
+  }
+]);
